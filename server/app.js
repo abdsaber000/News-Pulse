8a3631ff9398f1c015481a20575a5d4891af680f
Anas Maged <ansmaged5@gmail.com>
2023-07-15 15:55:49 +0300

    app changes

--- server/app.js
+++ server/app.js
@@ -2,13 +2,14 @@ import express from "express";
 import dotenv from "dotenv";
 import {connectDB}  from "./db/connect.js";
 import {startAdmin} from "./admin/index.js";
-
+import {router} from "./routes/news.js"
+import {Blog} from "./models/Blog.js"
 const app = express();
 dotenv.config();
 
 // middleware
 app.use(express.json());
-
+app.use('api/v1/news' , router)
 
 const port = process.env.PORT || 3000;
 
@@ -16,7 +17,8 @@ const start = async () => {
   try {
     // connectDB
     await connectDB(process.env.MONGO_URI);
-    startAdmin(app);
+    startAdmin(app); 
+
     app.listen(port, () => console.log(`Server is listening port ${port}...`));
   } catch (error) {
     console.log(error);
