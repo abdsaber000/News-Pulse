2f00361f3beb51553effe86afd8ead9a1bdb2765
Anas Maged <ansmaged5@gmail.com>
2023-07-19 08:42:34 +0300

    Updating controllers {get all , get one , create , update , delete}

--- server/controllers/news.js
+++ server/controllers/news.js
@@ -1,45 +1,62 @@
 import express from "express";
-import statusCodes from "http-status-codes"
+import statusCodes from "http-status-codes";
+import {Blog} from "../models/Blog.js"; 
+
 export const getAllBlogs = async (req , res)=>{
     try{
-        res.status(statusCodes.ACCEPTED).send("get all news");
+        const blogs = await Blog.find({});
+        res.status(statusCodes.ACCEPTED).json(blogs);
     }catch(error){
-        res.status(statusCodes.BAD_REQUEST).send(error);
+        res.status(statusCodes.BAD_REQUEST).json({msg:error});
     }
 }
+
 
 export const getBlog = async (req , res)=>{
     try{
         const BlogId = req.params.id;
-        res.status(statusCodes.ACCEPTED).send(`Get Id ${BlogId}`);
+        const blog = await Blog.findOne({_id:BlogId})
+        if(!blog){
+            return res.status(statusCodes.NOT_FOUND).json(`No Blog found with id ${BlogId}`);
+        }
+        res.status(statusCodes.ACCEPTED).json({blog});
     }catch(error){
-        res.status(statusCodes.BAD_REQUEST).send(error);
+        res.status(statusCodes.BAD_REQUEST).json({msg:error});
     }
 }
 
 export const createBlog = async (req , res)=>{
     try{
-        res.status(statusCodes.ACCEPTED).send(`Blog created`);
+        const blog = await Blog.create(req.body)
+        res.status(statusCodes.ACCEPTED).json({blog});
     }catch(error){
-        res.status(statusCodes.BAD_REQUEST).send(error);
+        res.status(statusCodes.BAD_REQUEST).json({msg:error});
     }
 }
 
 export const updateBlog = async (req , res)=>{
     try{
         const BlogId = req.params.id;
-        res.status(statusCodes.ACCEPTED).send(`update Id ${BlogId}`);
+        const blog = await Blog.findOneAndUpdate({_id:BlogId} , req.body)
+        if(!blog){
+            return res.status(statusCodes.NOT_FOUND).json(`No Blog found with id ${BlogId}`);
+        }
+        res.status(statusCodes.ACCEPTED).json({blog});
     }catch(error){
-        res.status(statusCodes.BAD_REQUEST).send(error);
+        res.status(statusCodes.BAD_REQUEST).json({msg:error});
     }
 }
 
 export const deleteBlog = async (req , res)=>{
     try{
         const BlogId = req.params.id;
-        res.status(statusCodes.ACCEPTED).send(`Delte Id ${BlogId}`);
+        const blog = await Blog.findOneAndDelete({_id:BlogId})
+        if(!blog){
+            return res.status(statusCodes.NOT_FOUND).json(`No Blog found with id ${BlogId}`);
+        }
+        res.status(statusCodes.ACCEPTED).json('Blog has been deleted successfully');
     }catch(error){
-        res.status(statusCodes.BAD_REQUEST).send(error);
+        res.status(statusCodes.BAD_REQUEST).json({msg:error});
     }
 }
 
