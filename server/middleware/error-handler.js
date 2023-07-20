7a290d17aa3b4bc60e0b6fecde53368690e7f45f
Abdulrahman Saber <abdsaber000@gmail.com>
2023-07-20 19:16:00 +0300

    Fix imports in error-handler middleware

--- server/middleware/error-handler.js
+++ server/middleware/error-handler.js
@@ -1,4 +1,4 @@
-const { StatusCodes } = require('http-status-codes')
+import { statusCodes } from 'http-status-codes';
 const errorHandlerMiddleware = (err, req, res, next) => {
   let customError = {
     // set default
