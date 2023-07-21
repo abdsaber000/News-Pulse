8c6e82c8283b3d298016269d83167cdbdd2dfe52
Abdulrahman Saber <abdsaber000@gmail.com>
2023-07-21 20:02:46 +0300

    Add validators to the User model

--- server/models/User.js
+++ server/models/User.js
@@ -2,15 +2,26 @@ import mongoose from "mongoose";
 
 export const UserSchema = new mongoose.Schema({
     user_name : {
-        type : String
+        type : String,
+        required : [true, 'You must provide a user name'],
+        unique : [true,  'The user name must be unique']
     },
 
     password : {
-        type : String
+        type : String,
+        required : [true, 'You must proivde a user name'],
+        minlength : 8
     },
 
     email : {
-        type: String
+        type: String,
+        required : [true , 'You must provide an email'],
+        match: [
+            /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+            'Please provide a valid email',
+          ],
+        unique : [true , 'Email must be unique']
+
     },
 })
 
