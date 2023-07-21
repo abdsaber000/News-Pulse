5137af7c40a2bef0e12cefdd25598417c40660b9
Abdulrahman Saber <abdsaber000@gmail.com>
2023-07-22 00:47:02 +0300

    Implement creatJWT to return token when User is created

--- server/models/User.js
+++ server/models/User.js
@@ -30,4 +30,14 @@ UserSchema.pre('save', async function () {
     this.password = await bcrypt.hash(this.password, salt)
 })
 
+UserSchema.methods.createJWT = function () {
+    return jwt.sign(
+      { userId: this._id, name: this.user_name },
+      process.env.JWT_SECRET,
+      {
+        expiresIn: process.env.JWT_LIFETIME,
+      }
+    )
+  }
+
 export const User =  mongoose.model('User' , UserSchema);
