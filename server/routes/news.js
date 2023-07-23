a41d85a7c3e4eac02b7d47b5c45794d55e55846f
Esraa Abdelgaber <100946188+EsraaAGaber@users.noreply.github.com>
2023-07-23 03:43:33 +0300

    Updating router getAllblogs to check the token first

--- server/routes/news.js
+++ server/routes/news.js
@@ -1,8 +1,8 @@
 import express from 'express'
-import {getAllBlogs , getBlog , updateBlog , deleteBlog , createBlog} from "../controllers/news.js"
+import {IsPublisher , getBlog , updateBlog , deleteBlog , createBlog} from "../controllers/news.js"
 const router = express.Router()
 
-router.route('/').get(getAllBlogs).post(createBlog);
+router.route('/').get(IsPublisher).post(createBlog);
 
 router.route('/:id').get(getBlog).patch(updateBlog).delete(deleteBlog);
 
