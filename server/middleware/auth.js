45d09b93bf11b8da32decbde74ae4f1eeab486b1
Abdulrahman Saber <abdsaber000@gmail.com>
2023-07-28 00:07:40 +0300

    Fix exception throwing in auth middleware

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -1,21 +1,22 @@
 import UnauthenticatedError from '../errors/unauthenticated.js';
 import jsonwebtoken from 'jsonwebtoken';
-
+import {errorHandler} from "../errors/error-handler.js";
 const auth = async (req, res, next) => {
-    // check header
-    const authHeader = req.headers.authorization
-    if (!authHeader || !authHeader.startsWith('Bearer')) {
-        throw new UnauthenticatedError('Authentication invalid')
-    }
-    const token = authHeader.split(' ')[1]
-  
-    try { 
+    try{
+        // check header
+        const authHeader = req.headers.authorization
+        if (!authHeader || !authHeader.startsWith('Bearer')) {
+            throw new UnauthenticatedError('Authentication invalid')
+        }
+        const token = authHeader.split(' ')[1]
+    
+    
         const payload = jsonwebtoken.verify(token, process.env.JWT_SECRET)
         // attach the user to the job routes
         req.user = { userId: payload.userId, name: payload.name }
         next()
     } catch (error) {
-        throw new UnauthenticatedError('Authentication invalid')
+        errorHandler(error , res);
     }
 }
 
