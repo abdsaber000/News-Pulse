9a789d3dc650ad883b6e43573a9b2a744e94f030
Esraa Abdelgaber <100946188+EsraaAGaber@users.noreply.github.com>
2023-08-03 00:42:56 +0300

    Add validation to report  model

--- server/models/contact.js
+++ server/models/contact.js
@@ -4,10 +4,17 @@ export const ReportSchema = new mongoose.Schema({
   
   email: {
     type: String,
+    required: [true, "You must provide an email"],
+    match: [
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+      "Please provide a valid email",
+    ],
   },
 
-  "report": {
+  report: {
     type: String,
+    required: [true, "The report Can't be empty"],
+
   },
 });
 
