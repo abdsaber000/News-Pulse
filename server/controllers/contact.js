1331844dbbf89a2af2d9c4ba656bec848ec7f519
Esraa Abdelgaber <100946188+EsraaAGaber@users.noreply.github.com>
2023-08-05 02:03:37 +0300

    Refactor data format in contact

--- server/controllers/contact.js
+++ server/controllers/contact.js
@@ -2,8 +2,8 @@ import { NewReport } from "../models/contact.js";
 import statusCodes from "http-status-codes";
 export const addreport = async (req, res) => {
   try {
-    const addnewReport = await NewReport.create(req.body);
-    res.status(statusCodes.ACCEPTED).json({ addnewReport });
+    const data = await NewReport.create(req.body);
+    res.status(statusCodes.ACCEPTED).json({ data });
   } catch (error) {
     res.status(statusCodes.BAD_REQUEST).json({ msg: error });
   }
